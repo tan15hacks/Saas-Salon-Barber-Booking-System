a6713db8309994e9c37d69cf531635b95d634722
tan15hacks <110644530+tan15hacks@users.noreply.github.com>
2026-07-07 17:06:40 +0800

    Remove public navbar from admin route

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,16 +1,12 @@
 import { AdminLoginGate } from "@/components/admin/AdminLoginGate";
 import { AdminWorkspaceSmart } from "@/components/admin/AdminWorkspaceSmart";
-import { Footer } from "@/components/site/Footer";
-import { Navbar } from "@/components/site/Navbar";
 
 export default function AdminPage() {
   return (
     <main className="min-h-screen bg-cream text-espresso">
-      <Navbar />
       <AdminLoginGate>
         <AdminWorkspaceSmart />
       </AdminLoginGate>
-      <Footer />
     </main>
   );
 }
